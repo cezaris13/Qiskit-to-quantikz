cc440fea56ccae229a0f2177af93af9a00e680a8
Pijus <pijus.petkevicius@bentley.com>
2025-06-15 18:06:42 +0200

    dummy test

--- tests/qiskit_to_quantikz_tests.py
+++ tests/qiskit_to_quantikz_tests.py
@@ -1,1 +1,12 @@
 #!/usr/bin/env python3
+
+from absl.testing import absltest
+
+
+class QuantikzTests(absltest.TestCase):
+    def test_quantikz(self):
+        pass
+
+
+if __name__ == "__main__":
+    absltest.main()
