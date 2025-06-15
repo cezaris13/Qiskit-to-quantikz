f13ec8e4bc037e18fd7128fdd03f384d9433c155
Pijus <pijus.petkevicius@bentley.com>
2025-06-15 20:28:30 +0200

    slice fixes

--- qiskit_quantikz.py
+++ qiskit_quantikz.py
@@ -35,9 +35,7 @@ def _init_lines(sub: QuantumCircuit, include_clbits: bool):
     return q_lines, c_lines
 
 
-def _add_slices(
-    token_slice: str, col: int, slice_all: bool, slice_titles: Optional[Dict[int, str]]
-):
+def _add_slices(col: int, slice_all: bool, slice_titles: Optional[Dict[int, str]]):
     if slice_all:
         return rf"\slice{{{col+1}}}"
     if slice_titles and col in slice_titles:
@@ -51,15 +49,8 @@ def _render_measure(
     measured = {sub.qubits.index(q) for q in qargs}
     for i in range(sub.num_qubits):
         q_lines[i].append((r"\meter{}" if i in measured else r"\qw") + token_slice)
-    # Classical bits only if drawn
     for j in range(len(c_lines)):
-        # check if this clbit is written to by any measurement
-        # map c_lines index j to sub.clbits index
-        written = False
-        for idx, c in enumerate(cargs):
-            if sub.clbits.index(c) == j:
-                written = True
-                break
+        written = any(sub.clbits.index(c) == j for c in cargs)
         c_lines[j].append((r"\cw" if written else r"\qw") + token_slice)
 
 
@@ -79,9 +70,7 @@ def _render_swap(qargs, sub: QuantumCircuit, q_lines, c_lines, token_slice: str)
 def _render_cswap(
     instr, qargs, sub: QuantumCircuit, q_lines, c_lines, token_slice: str
 ):
-    ctrl = sub.qubits.index(qargs[0])
-    s0 = sub.qubits.index(qargs[1])
-    s1 = sub.qubits.index(qargs[2])
+    ctrl, s0, s1 = [sub.qubits.index(q) for q in qargs]
     for i in range(sub.num_qubits):
         if i == ctrl:
             off = s0 - ctrl
@@ -126,7 +115,6 @@ def _render_two_qubit(
     ctrl, tgt = sub.qubits.index(qargs[0]), sub.qubits.index(qargs[1])
     name = instr.name.lower()
     params = getattr(instr, "params", [])
-    # Handle controlled rotation gates with parameters
     if name in ("crx", "cry", "crz") and params:
         axis = name[-1]
         theta = params[0]
@@ -137,22 +125,22 @@ def _render_two_qubit(
                 q_lines[i].append(rf"\gate{{R_{axis}({theta})}}" + token_slice)
             else:
                 q_lines[i].append(r"\qw" + token_slice)
-        return
-    for i in range(sub.num_qubits):
-        if name in ("cx", "cnot") and i == ctrl:
-            q_lines[i].append(rf"\ctrl{{{tgt-ctrl}}}" + token_slice)
-        elif name in ("cx", "cnot") and i == tgt:
-            q_lines[i].append(r"\targ{}" + token_slice)
-        elif name == "cz" and i == ctrl:
-            q_lines[i].append(rf"\ctrl{{{tgt-ctrl}}}" + token_slice)
-        elif name == "cz" and i == tgt:
-            q_lines[i].append(rf"\ctrl{{{ctrl-tgt}}}" + token_slice)
-        elif i == ctrl:
-            q_lines[i].append(rf"\ctrl{{{tgt-ctrl}}}" + token_slice)
-        elif i == tgt:
-            q_lines[i].append(rf"\gate{{{instr.name.upper()}}}" + token_slice)
-        else:
-            q_lines[i].append(r"\qw" + token_slice)
+    else:
+        for i in range(sub.num_qubits):
+            if name in ("cx", "cnot") and i == ctrl:
+                q_lines[i].append(rf"\ctrl{{{tgt-ctrl}}}" + token_slice)
+            elif name in ("cx", "cnot") and i == tgt:
+                q_lines[i].append(r"\targ{}" + token_slice)
+            elif name == "cz" and i == ctrl:
+                q_lines[i].append(rf"\ctrl{{{tgt-ctrl}}}" + token_slice)
+            elif name == "cz" and i == tgt:
+                q_lines[i].append(rf"\ctrl{{{ctrl-tgt}}}" + token_slice)
+            elif i == ctrl:
+                q_lines[i].append(rf"\ctrl{{{tgt-ctrl}}}" + token_slice)
+            elif i == tgt:
+                q_lines[i].append(rf"\gate{{{instr.name.upper()}}}" + token_slice)
+            else:
+                q_lines[i].append(r"\qw" + token_slice)
     for _ in range(len(c_lines)):
         c_lines[_].append(r"\qw" + token_slice)
 
@@ -161,9 +149,7 @@ def _render_default(
     instr, qargs, sub: QuantumCircuit, q_lines, c_lines, token_slice: str
 ):
     name = instr.name.lower()
-    targets = {sub.qubits.index(q) for q in qargs}
     params = getattr(instr, "params", [])
-    # Handle single-qubit rotation gates with parameters
     if len(qargs) == 1 and name in ("rx", "ry", "rz") and params:
         axis = name[-1]
         theta = params[0]
@@ -175,6 +161,7 @@ def _render_default(
         for _ in range(len(c_lines)):
             c_lines[_].append(r"\qw" + token_slice)
         return
+    targets = {sub.qubits.index(q) for q in qargs}
     for i in range(sub.num_qubits):
         if i in targets:
             q_lines[i].append(rf"\gate{{{instr.name.upper()}}}" + token_slice)
@@ -191,11 +178,11 @@ def render(
     slice_all: bool = False,
 ) -> str:
     """
-    Render a single QuantumCircuit subcircuit into Quantikz LaTeX.
+    Render a single QuantumCircuit subcircuit into Quantikz LaTeX, with optional in-place slice annotations.
     """
     q_lines, c_lines = _init_lines(sub, include_clbits)
     for col, (instr, qargs, cargs) in enumerate(sub.data):
-        token_slice = _add_slices("", col, slice_all, slice_titles)
+        token_slice = _add_slices(col, slice_all, slice_titles)
         name = instr.name.lower()
         if name == "measure":
             _render_measure(instr, qargs, cargs, sub, q_lines, c_lines, token_slice)
@@ -215,9 +202,19 @@ def render(
         for j, line in enumerate(c_lines):
             line.insert(0, rf"\lstick{{${{c_{j}}}$}}")
     rows = [" & ".join(l) + r" & \\" for l in q_lines]
-    rows += [" & ".join(l) + r" & \\" for l in c_lines] if c_lines else []
+    if c_lines:
+        rows += [" & ".join(l) + r" & \\" for l in c_lines]
     body = "\n".join(rows)
-    return "\n".join([r"\begin{quantikz}", body, r"\end{quantikz}"])
+    return remove_last_occurrence(
+        "\n".join([r"\begin{quantikz}", body, r"\end{quantikz}"]), r" \\"
+    )
+
+
+def remove_last_occurrence(s, sub):
+    index = s.rfind(sub)
+    if index == -1:
+        return s
+    return s[:index] + s[index + len(sub) :]
 
 
 def qiskit_to_quantikz(
@@ -229,14 +226,20 @@ def qiskit_to_quantikz(
     slice_all: bool = False,
 ) -> Union[str, List[str]]:
     """
-    Convert a Qiskit QuantumCircuit into quantikz LaTeX, supporting various gates.
+    Convert a Qiskit QuantumCircuit into quantikz LaTeX.
+    - If slice_all or slice_titles is used, returns one string with in-place \\slice annotations.
+    - Otherwise, if slice_indices or n_slices>1, returns a list of subcircuit strings.
     """
-    if slice_indices:
-        bounds = sorted({0, *slice_indices, len(qc.data)})
-        subcircs = split_circuit_by_indices(qc, bounds)
-    elif n_slices and n_slices > 1:
-        subcircs = split_circuit_by_count(qc, n_slices)
-    else:
-        subcircs = [qc]
-    outputs = [render(s, include_clbits, slice_titles, slice_all) for s in subcircs]
-    return outputs if len(outputs) > 1 else outputs[0]
+    # In-place slicing has priority
+    if slice_all or (slice_titles and len(slice_titles) > 0):
+        return render(qc, include_clbits, slice_titles, slice_all)
+    # If splitting requested, do legacy behavior
+    if slice_indices or (n_slices and n_slices > 1):
+        if slice_indices:
+            bounds = sorted({0, *slice_indices, len(qc.data)})
+            subcircs = split_circuit_by_indices(qc, bounds)
+        else:
+            subcircs = split_circuit_by_count(qc, n_slices)
+        return [render(s, include_clbits, slice_titles, slice_all) for s in subcircs]
+    # Default single circuit
+    return render(qc, include_clbits, slice_titles, slice_all)
--- tests/qiskit_to_quantikz_tests.py
+++ tests/qiskit_to_quantikz_tests.py
@@ -9,7 +9,7 @@ import qiskit_quantikz as qq
 class QuantikzTests(absltest.TestCase):
     def test_xgate(self):
         actual_result = r"""\begin{quantikz}
-\lstick{${q_0}$} & \gate{X} & \\
+\lstick{${q_0}$} & \gate{X} &
 \end{quantikz}"""
 
         circuit = QuantumCircuit(1)
@@ -21,7 +21,7 @@ class QuantikzTests(absltest.TestCase):
 
     def test_ygate(self):
         actual_result = r"""\begin{quantikz}
-\lstick{${q_0}$} & \gate{Y} & \\
+\lstick{${q_0}$} & \gate{Y} &
 \end{quantikz}"""
 
         circuit = QuantumCircuit(1)
@@ -34,7 +34,7 @@ class QuantikzTests(absltest.TestCase):
     def test_cxgate(self):
         actual_result = r"""\begin{quantikz}
 \lstick{${q_0}$} & \ctrl{1} & \\
-\lstick{${q_1}$} & \targ{} & \\
+\lstick{${q_1}$} & \targ{} &
 \end{quantikz}"""
 
         circuit = QuantumCircuit(2)
@@ -47,7 +47,7 @@ class QuantikzTests(absltest.TestCase):
     def test_xcgate(self):
         actual_result = r"""\begin{quantikz}
 \lstick{${q_0}$} & \targ{} & \\
-\lstick{${q_1}$} & \ctrl{-1} & \\
+\lstick{${q_1}$} & \ctrl{-1} &
 \end{quantikz}"""
 
         circuit = QuantumCircuit(2)
@@ -60,7 +60,7 @@ class QuantikzTests(absltest.TestCase):
     def test_czgate(self):
         actual_result = r"""\begin{quantikz}
 \lstick{${q_0}$} & \ctrl{1} & \\
-\lstick{${q_1}$} & \ctrl{-1} & \\
+\lstick{${q_1}$} & \ctrl{-1} &
 \end{quantikz}"""
 
         circuit = QuantumCircuit(2)
@@ -73,7 +73,7 @@ class QuantikzTests(absltest.TestCase):
         actual_result = r"""\begin{quantikz}
 \lstick{${q_0}$} & \ctrl{2} & \\
 \lstick{${q_1}$} & \qw & \\
-\lstick{${q_2}$} & \ctrl{-2} & \\
+\lstick{${q_2}$} & \ctrl{-2} &
 \end{quantikz}"""
 
         circuit = QuantumCircuit(3)
@@ -84,7 +84,7 @@ class QuantikzTests(absltest.TestCase):
 
     def test_rxgate(self):
         actual_result = r"""\begin{quantikz}
-\lstick{${q_0}$} & \gate{R_x(0.5)} & \\
+\lstick{${q_0}$} & \gate{R_x(0.5)} &
 \end{quantikz}"""
 
         circuit = QuantumCircuit(1)
@@ -96,7 +96,7 @@ class QuantikzTests(absltest.TestCase):
     def test_swap(self):
         actual_result = r"""\begin{quantikz}
 \lstick{${q_0}$} & \swap{1} & \\
-\lstick{${q_1}$} & \targX{} & \\
+\lstick{${q_1}$} & \targX{} &
 \end{quantikz}"""
 
         circuit = QuantumCircuit(2)
@@ -109,7 +109,7 @@ class QuantikzTests(absltest.TestCase):
         actual_result = r"""\begin{quantikz}
 \lstick{${q_0}$} & \swap{2} & \\
 \lstick{${q_1}$} & \qw & \\
-\lstick{${q_2}$} & \targX{} & \\
+\lstick{${q_2}$} & \targX{} &
 \end{quantikz}"""
 
         circuit = QuantumCircuit(3)
@@ -123,7 +123,7 @@ class QuantikzTests(absltest.TestCase):
         actual_result = r"""\begin{quantikz}
 \lstick{${q_0}$} & \ctrl{2} & \\
 \lstick{${q_1}$} & \ctrl{1} & \\
-\lstick{${q_2}$} & \targ{} & \\
+\lstick{${q_2}$} & \targ{} &
 \end{quantikz}"""
 
         circuit = QuantumCircuit(3)
@@ -136,7 +136,7 @@ class QuantikzTests(absltest.TestCase):
         actual_result = r"""\begin{quantikz}
 \lstick{${q_0}$} & \ctrl{2} & \\
 \lstick{${q_1}$} & \ctrl{1} & \\
-\lstick{${q_2}$} & \ctrl{0} & \\
+\lstick{${q_2}$} & \ctrl{0} &
 \end{quantikz}"""
 
         circuit = QuantumCircuit(3)
@@ -148,7 +148,7 @@ class QuantikzTests(absltest.TestCase):
     def test_crxgate(self):
         actual_result = r"""\begin{quantikz}
 \lstick{${q_0}$} & \ctrl{1} & \\
-\lstick{${q_1}$} & \gate{R_x(0.5)} & \\
+\lstick{${q_1}$} & \gate{R_x(0.5)} &
 \end{quantikz}"""
 
         circuit = QuantumCircuit(2)
@@ -161,7 +161,7 @@ class QuantikzTests(absltest.TestCase):
         actual_result = r"""\begin{quantikz}
 \lstick{${q_0}$} & \ctrl{1} & \\
 \lstick{${q_1}$} & \swap{1} & \\
-\lstick{${q_2}$} & \targX{} & \\
+\lstick{${q_2}$} & \targX{} &
 \end{quantikz}"""
 
         circuit = QuantumCircuit(3)
@@ -176,7 +176,7 @@ class QuantikzTests(absltest.TestCase):
 \lstick{${q_1}$} & \qw & \\
 \lstick{${q_2}$} & \swap{2} & \\
 \lstick{${q_3}$} & \qw & \\
-\lstick{${q_4}$} & \targX{} & \\
+\lstick{${q_4}$} & \targX{} &
 \end{quantikz}"""
 
         circuit = QuantumCircuit(5)
@@ -188,7 +188,7 @@ class QuantikzTests(absltest.TestCase):
     def test_cx_measure_no_cl_bits(self):
         actual_result = r"""\begin{quantikz}
 \lstick{${q_0}$} & \ctrl{1} & \meter{} & \qw & \\
-\lstick{${q_1}$} & \targ{} & \qw & \meter{} & \\
+\lstick{${q_1}$} & \targ{} & \qw & \meter{} &
 \end{quantikz}"""
 
         circuit = QuantumCircuit(2, 2)
@@ -202,7 +202,7 @@ class QuantikzTests(absltest.TestCase):
 \lstick{${q_0}$} & \ctrl{1} & \meter{} & \qw & \\
 \lstick{${q_1}$} & \targ{} & \qw & \meter{} & \\
 \lstick{${c_0}$} & \qw & \cw & \qw & \\
-\lstick{${c_1}$} & \qw & \qw & \cw & \\
+\lstick{${c_1}$} & \qw & \qw & \cw &
 \end{quantikz}"""
 
         circuit = QuantumCircuit(2, 2)
@@ -210,6 +210,50 @@ class QuantikzTests(absltest.TestCase):
         circuit.measure([0, 1], [0, 1])
         result = qq.qiskit_to_quantikz(circuit, include_clbits=True)
         self.assertEqual(result, actual_result)
+
+    # -- New slicing tests --
+    def test_split_circuit_by_indices_basic(self):
+        qc = QuantumCircuit(2)
+        qc.h(0)
+        qc.cx(0, 1)
+        qc.x(1)
+        subs = qq.split_circuit_by_indices(qc, [0, 1, 3])
+        self.assertEqual(len(subs), 2)
+        self.assertEqual(subs[0].data[0][0].name.lower(), "h")
+        self.assertEqual(subs[1].data[0][0].name.lower(), "cx")
+        self.assertEqual(subs[1].data[1][0].name.lower(), "x")
+
+    def test_split_circuit_by_count_even(self):
+        qc = QuantumCircuit(1)
+        for _ in range(4):
+            qc.x(0)
+        subs = qq.split_circuit_by_count(qc, 2)
+        self.assertEqual(len(subs), 2)
+        self.assertTrue(all(len(s.data) == 2 for s in subs))
+
+    def test_slicing_all(self):
+        qc = QuantumCircuit(1)
+        qc.h(0)
+        qc.x(0)
+        qc.y(0)
+        latex = qq.qiskit_to_quantikz(qc, slice_all=True)
+        self.assertIn("\\slice{1}", latex)
+        self.assertIn("\\slice{2}", latex)
+        self.assertIn("\\slice{3}", latex)
+
+        self.assertTrue(latex.strip().endswith("\\end{quantikz}"))
+        self.assertFalse(latex.strip().endswith("& \\\\n\\end{quantikz}"))
+
+    def test_slice_titles(self):
+        qc = QuantumCircuit(1)
+        qc.h(0)
+        qc.x(0)
+        qc.y(0)
+        titles = {0: "init", 1: "middle", 2: "end"}
+        latex = qq.qiskit_to_quantikz(qc, slice_titles=titles)
+        self.assertIn("\\slice{init}", latex)
+        self.assertIn("\\slice{middle}", latex)
+        self.assertIn("\\slice{end}", latex)
 
 
 if __name__ == "__main__":
